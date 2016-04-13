b5ca4781f76e5340417035fd47b7972e5aaa6ed7
Olesya <Olesya>
2016-04-13 16:17:55 +0400

    first commit

---
+++ JS/index.js
@@ -0,0 +1,99 @@
+"is strict"
+
+$(document).ready(function() {
+	loadJSON('./PY/data_week.json');	
+	$(document).tooltip({
+		item: ".channel_program_item_name_inner",
+		position: {my: "left center", at: "right+10 center"},
+		tooltipClass: "tooltip",
+		show: {effect: 'none', delay:400},
+		hide: {effect: 'fade', delay:400, duration:600},
+		content: function(callback) {
+			var elem = $(this);
+			console.log($(elem).text());
+			var tooltipHTML = $('<div/>').append("Подробная информация о программе <div class='tooltip_program'>" + $(elem).text() + "</div></div>");
+			return tooltipHTML;
+		}
+	});
+});
+
+var day = 'today'
+var channels = [];
+var data = [];
+var dates = [];
+var weekdays = [];
+
+function loadJSON(url) {
+	$.getJSON(url, function(jsonData) {
+		data = jsonData['data'];
+	}).done(function(jsonData) {
+		loadChannels();
+		loadLinks();
+	})
+	.fail(function() {
+		console.log("json load error");
+	})
+}
+
+function loadChannels() {
+	$('#channels').empty();
+	channels = data[day]['channels'];
+	for (var i = 0; i < channels.length; i++) {
+		var el = $("<div/>", {class : "channel"});
+		var name = $("<div/>", {class : "channel_title", html: channels[i]['channel']});
+		var label = $("<div/>", {class : "channel_name"});
+		var icon = $("<div/>", {class : "channel_icon"});
+		icon.css("background-image", 'url("' + channels[i]['icon'] + '")');
+		
+		icon.appendTo(label);
+		name.appendTo(label);
+		$(label).appendTo(el);
+
+		var program = $("<div/>", {class : "channel_program"});
+		for (var j = 0; j < channels[i]['programs'].length; j++) {
+		 	var program_item = $("<div/>", {class : "channel_program_item"});
+			var program_item_time = $("<div/>", {class : "channel_program_item_time", "html": channels[i]['programs'][j][0]});
+			var program_name = "<span class='channel_program_item_name_inner' title=''>" + channels[i]['programs'][j][1] + "</span>";
+			var program_item_name = $("<div/>", {class : "channel_program_item_name", "html": program_name});
+			//$(program_item_name).attr('title',  "tooltip");
+	
+		 	program_item_time.appendTo(program_item);
+		 	program_item_name.appendTo(program_item);
+		 	program_item.appendTo(program);
+		}
+		$(program).appendTo(el);
+
+		el.appendTo("#channels");
+	}
+}
+
+function loadLinks() {
+	for (dt in data) {
+		if (data[dt]['weekday'] != 'today') {
+			dates.push(data[dt]['date']);
+			weekdays.push(data[dt]['weekday']);
+		}
+	}
+
+	var dayDivs = $('.nav_day_link');
+	for (var i = 0; i < 6; i++)	{
+		$(dayDivs[i + 1]).text(dates[i] + ', ' + weekdays[i]);
+	}
+}
+
+function selectLink() {
+	dayDivs = $('.nav_day');
+	$(dayDivs).removeClass('selected');
+	if (day == 'today')
+		$(dayDivs[0]).addClass('selected');
+	else
+		$(dayDivs[day]).addClass('selected');
+}
+
+
+function loadPrograms(day) {
+		this.day = day;
+		loadChannels();
+
+		selectLink();
+	}
---
+++ PY/reader_week.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib
+import lxml.html as html
+import io, json
+import datetime
+import codecs
+
+
+json_string = ''
+data = dict()
+
+def weekdayToString(weekday):
+	return {
+		0: u"Пн",
+		1: u"Вт",
+		2: u"Ср",
+		3: u"Чт",
+		4: u"Пт",
+		5: u"Сб",
+		6: u"Вс",
+		'today': u"Сегодня"
+	}[weekday]
+
+def readDay(ind, link, day, weekday):
+	connection = urllib.urlopen(link)
+	st = connection.read()
+	connection.close()
+	page = html.fromstring(st)
+	channels = page.cssselect('.tv-channel')
+	
+	file = codecs.open('page' + str(ind) + '.html', "w")#, "utf-8")
+	file.write(st)
+	file.close()
+	
+	global data
+	data[ind] = dict()
+	data[ind]['date'] = day
+	data[ind]['weekday'] = weekdayToString(weekday)
+	data[ind]['channels'] = []
+
+	for row in channels:
+		item = dict()
+		item['channel'] = row.cssselect('.tv-channel-title__text')[0].text_content()
+		style = row.cssselect('.b-tv-image__picture')[0].get("style").replace("url(", "url(http:")
+		item['icon'] = "http://" + style[style.find("avatars"):-1]
+		item['programs'] = []
+		if (len(row.cssselect('.tv-channel-events__items')) != 0):
+			pr = row.cssselect('.tv-channel-events__items')[0]
+			time = pr.cssselect('.tv-event__time-text')
+			prog = pr.cssselect('.tv-event__title-inner')
+			for p in range(0, len(time)):
+			 	item['programs'].append([time[p].text_content(), prog[p].text_content()])
+		
+		data[ind]['channels'].append(item)
+
+
+	# global json_string
+	# json_string += json.dumps({"data":data[day]}, indent=4, ensure_ascii = False).encode('utf-8')
+
+now = datetime.datetime.now()
+links = {'today': ["https://tv.yandex.ru/213?grid=main&period=all-day", now.strftime("%d"), now.weekday()]}
+one_day = datetime.timedelta(days=1)
+for i in range(1, 7):
+	now += one_day
+	links[i] = ["https://tv.yandex.ru/213?date=" + now.strftime("%Y-%m-%d") + "&grid=main&period=all-day", now.strftime("%d"), now.weekday()]
+
+for k, v in links.iteritems():
+	readDay(k, v[0], v[1], v[2])
+
+with open('data_week.json', 'w') as outfile:
+	json_string = json.dumps({"data":data}, indent=4, ensure_ascii = False).encode('utf-8')
+	outfile.write(json_string)
+
